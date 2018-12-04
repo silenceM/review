79cc3e42a9b53a4f510076a061a76a4880e6d198
silenceM <357003671@qq.com>
2018-12-04 10:59:00 +0800

    add Bubble flag

--- BubbleSort.js
+++ BubbleSort.js
@@ -1,10 +1,15 @@
 function BubbleSort(originArray) {
   const array = [...originArray]
+  let swapped = false;
   for (let i = 0; i < array.length; i += 1) {
     for (let l = 0; l < array.length - i; l += 1) {
       if (array[l] > array[l + 1]) {
+        swapped = true
         [array[l], array[l + 1]] = [array[l + 1], array[l]]
       }
+    }
+    if (!swapped) {
+      return array
     }
   }
   return array
