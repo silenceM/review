3645dc502ad87d1c0482d601a22e4ffb06374910
silenceM <357003671@qq.com>
2018-12-14 00:39:17 +0800

    update call2

--- call2.js
+++ call2.js
@@ -1,3 +1,23 @@
-function call2() {
-  
+Function.prototype.call2 = function (context) {
+  var context = context || window
+  context.fn = this
+  var args = []
+  for(var i = 1; i < arguments.length; i+=1) {
+    args.push('arguments[' + i + ']')
+  }
+  const result = eval('context.fn(' + args + ')')
+  delete context.fn
+  return result
 }
+
+var foo = {
+  value: 1
+}
+
+function bar(name, age) {
+  console.log(name)
+  console.log(age)
+  console.log(this.value)
+}
+
+bar.call2(foo, 'silence', 20)
