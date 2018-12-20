dca6ff2f20e6d9d85d390a15fc3c8245b75b9171
silenceM <357003671@qq.com>
2018-12-20 12:38:16 +0800

    update quickSort

--- QuickSort.js
+++ QuickSort.js
@@ -1,3 +1,37 @@
-function QuickSort() {
-  
+function QuickSort(
+  originalArray,
+  inputLowIndex = 0,
+  inputHeightIndex = originArray.length -1,
+  recursiveCall = false,
+) {
+  const array = recursiveCall ? originalArray : [...originalArray]
+  const partitionArray = (lowIndex, highIndex) => {
+    const swap = (leftIndex, rightIndex) => {
+      [
+        array[leftIndex],
+        array[rightIndex]
+      ] =[
+        array[rightIndex],
+        array[leftIndex]
+      ]
+    }
+    const pivot = array[highIndex]
+    let partitionIndex = lowIndex
+    for (let currentIndex = lowIndex; currentIndex < highIndex; currentIndex += 1) {
+      if (array[currentIndex] < pivot) {
+        swap(partitionIndex, currentIndex)
+        partitionIndex += 1;
+      }
+    }
+    swap(partitionIndex, highIndex)
+    return partitionIndex
+  }
+  if (inputLowIndex < inputHeightIndex) {
+    const partitionIndex = partitionArray(inputLowIndex, inputHeightIndex)
+    const RECURSIVE_CALL = true
+    QuickSort(array, inputLowIndex, partitionIndex - 1, RECURSIVE_CALL)
+    QuickSort(array, partitionIndex + 1, inputHeightIndex, RECURSIVE_CALL)
+  }
+  return array
 }
+// TODO TEST
